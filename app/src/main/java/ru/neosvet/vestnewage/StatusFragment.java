85ac124d7d2f553e58e678b45295dc9a14217686
NeoSvet <neosvet333@gmail.com>
2017-02-28 21:46:53 +0300

    нарисовал интерфейс к разделу "Состояние"

--- app/src/main/java/ru/neosvet/vestnewage/StatusFragment.java
+++ app/src/main/java/ru/neosvet/vestnewage/StatusFragment.java
@@ -1,18 +1,30 @@
 package ru.neosvet.vestnewage;
 
-import android.os.Bundle;
 import android.app.Fragment;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AdapterView;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import ru.neosvet.ui.ListAdapter;
+import ru.neosvet.ui.ListItem;
 
 public class StatusFragment extends Fragment {
 
     private MainActivity act;
     private ListAdapter adMain;
-    private View container, fabExit;
+    private CheckBox cbRemEmail, cbRemPassword;
+    private EditText etEmail, etPassword;
+    private View container, fabEnter, fabExit;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -20,7 +32,7 @@ public class StatusFragment extends Fragment {
         this.container = inflater.inflate(R.layout.status_fragment, container, false);
         act = (MainActivity) getActivity();
         initViews();
-//        setViews();
+        setViews();
 //        restoreActivityState(savedInstanceState);
         return this.container;
     }
@@ -34,6 +46,85 @@ public class StatusFragment extends Fragment {
 
     private void initViews() {
         act.setTitle(getResources().getString(R.string.status));
+        fabEnter = container.findViewById(R.id.fabEnter);
+        fabExit = container.findViewById(R.id.fabExit);
+        etEmail = (EditText) container.findViewById(R.id.etEmail);
+        etPassword = (EditText) container.findViewById(R.id.etPassword);
+        cbRemEmail = (CheckBox) container.findViewById(R.id.cbRemEmail);
+        cbRemPassword = (CheckBox) container.findViewById(R.id.cbRemPassword);
+        ListView lvList = (ListView) container.findViewById(R.id.lvList);
+        adMain = new ListAdapter(act);
+        for (int i = 0; i < getResources().getStringArray(R.array.status).length; i += 2) {
+            adMain.addItem(new ListItem(getResources().getStringArray(R.array.status)[i]),
+                    getResources().getStringArray(R.array.status)[i + 1]);
+        }
+        lvList.setAdapter(adMain);
+        lvList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int pos, long l) {
+
+            }
+        });
+    }
+
+    private void setViews() {
+        TextWatcher textWatcher = new TextWatcher() {
+            public void afterTextChanged(Editable s) {
+                boolean b = false;
+                if (etEmail.length() > 5 && etPassword.length() > 5) {
+                    b = (etEmail.getText().toString().contains("@") && etEmail.getText().toString().contains("."));
+                }
+                if (b) { //ready to login
+                    fabEnter.setVisibility(View.VISIBLE);
+                } else {
+                    fabEnter.setVisibility(View.GONE);
+                }
+            }
+
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+        };
+        etEmail.addTextChangedListener(textWatcher);
+        etPassword.addTextChangedListener(textWatcher);
+
+        etPassword.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int keyCode, KeyEvent keyEvent) {
+                if ((keyEvent.getAction() == KeyEvent.ACTION_DOWN && keyEvent.getKeyCode() == KeyEvent.KEYCODE_ENTER)
+                        || keyCode == EditorInfo.IME_ACTION_GO) {
+                    if (fabEnter.getVisibility() == View.VISIBLE)
+                        subLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
+        cbRemEmail.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean check) {
+                cbRemPassword.setEnabled(check);
+                if (!check)
+                    cbRemPassword.setChecked(false);
+            }
+        });
+        fabEnter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                subLogin();
+            }
+        });
+        fabExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+    }
+
+    private void subLogin() {
+
     }
 
 }
