cc2817bbc7e6273763843143ea73205a5a6572ff
NeoSvet <neosvet333@gmail.com>
2017-03-03 17:33:31 +0300

    реализация

--- app/src/main/java/ru/neosvet/vestnewage/ViewerActivity.java
+++ app/src/main/java/ru/neosvet/vestnewage/ViewerActivity.java
@@ -1,13 +1,82 @@
 package ru.neosvet.vestnewage;
 
-import android.support.v7.app.AppCompatActivity;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.webkit.ValueCallback;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import ru.neosvet.ui.StatusBar;
+import ru.neosvet.utils.DataBase;
 
 public class ViewerActivity extends AppCompatActivity {
+    private final String HOST = "http://o53xo.n52gw4tpozsw42lzmexgk5i.cmle.ru/", SCRIPT =
+            "var s=document.getElementById('rcol').innerHTML;s=s.substring(s.indexOf('/d')+5);s=s.substring(0,s.indexOf('lnk')-34);document.body.innerHTML='<div id=\"rcol\" style=\"padding-top:20px\">'+s+'</div>';";
+    private WebView wvBrowser;
+    private StatusBar status;
+
+    public static void openPage(Context context, String link) {
+        Intent intent = new Intent(context, ViewerActivity.class);
+        intent.putExtra(DataBase.LINK, link);
+        if (!(context instanceof Activity))
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.viewer_activity);
+        initView();
+        String link = getIntent().getStringExtra(DataBase.LINK);
+        wvBrowser.loadUrl(HOST + link);
+    }
+
+    @SuppressLint("JavascriptInterface")
+    private void initView() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        this.setTitle("");
+        wvBrowser = (WebView) findViewById(R.id.wvBrowser);
+        wvBrowser.getSettings().setJavaScriptEnabled(true);
+        wvBrowser.setWebViewClient(new wvClient());
+        status = new StatusBar(this, findViewById(R.id.pStatus));
+    }
+
+    private class wvClient extends WebViewClient {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            status.setLoad(true);
+            ViewerActivity.this.setTitle("");
+            view.setVisibility(View.GONE);
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            if (android.os.Build.VERSION.SDK_INT > 18) {
+                wvBrowser.evaluateJavascript(SCRIPT,
+                        new ValueCallback<String>() {
+                            @Override
+                            public void onReceiveValue(String s) {
+                                wvBrowser.setVisibility(View.VISIBLE);
+                            }
+                        });
+            } else {
+                wvBrowser.loadUrl("javascript: " + SCRIPT + " return false;");
+                wvBrowser.setVisibility(View.VISIBLE);
+            }
+            String s = wvBrowser.getTitle();
+            ViewerActivity.this.setTitle(s.substring(s.indexOf(":") + 3));
+            status.setLoad(false);
+            super.onPageFinished(view, url);
+        }
     }
 }
