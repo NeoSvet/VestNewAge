2f0bcf7312ea1f6959dc57331badda6f7aaa4c9b
NeoSvet <neosvet333@gmail.com>
2017-03-31 09:45:27 +0300

    развертывание на весь экран

--- app/src/main/java/ru/neosvet/vestnewage/SlashActivity.java
+++ app/src/main/java/ru/neosvet/vestnewage/SlashActivity.java
@@ -10,6 +10,8 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 
@@ -35,6 +37,10 @@ public class SlashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.slash_activity);
         main = new Intent(getApplicationContext(), MainActivity.class);
         status = new StatusBar(this, findViewById(R.id.pStatus));
