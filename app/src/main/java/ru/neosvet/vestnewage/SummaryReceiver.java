2048d8d963f161d1710ccde22d36980a8c500403
NeoSvet <neosvet333@gmail.com>
2017-04-06 19:25:19 +0300

    исправлена и изменена работа уведомления: открытие ссылки

--- app/src/main/java/ru/neosvet/vestnewage/SummaryReceiver.java
+++ app/src/main/java/ru/neosvet/vestnewage/SummaryReceiver.java
@@ -1,6 +1,7 @@
 package ru.neosvet.vestnewage;
 
 import android.app.AlarmManager;
+import android.app.IntentService;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
@@ -9,6 +10,7 @@ import android.content.SharedPreferences;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.media.RingtoneManager;
+import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.WakefulBroadcastReceiver;
 
@@ -19,14 +21,14 @@ import org.apache.http.impl.client.DefaultHttpClient;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Date;
 
 import ru.neosvet.utils.Lib;
-
-import static android.content.Context.MODE_PRIVATE;
 
 public class SummaryReceiver extends WakefulBroadcastReceiver {
     private static final int notif_id = 111;
@@ -48,65 +50,108 @@ public class SummaryReceiver extends WakefulBroadcastReceiver {
     }
 
     @Override
-    public void onReceive(final Context context, Intent intent) {
-        SharedPreferences pref = context.getSharedPreferences(SettingsFragment.SUMMARY, MODE_PRIVATE);
-        final int p = pref.getInt(SettingsFragment.TIME, -1);
-        if (p == -1)
-            return;
-        final boolean boolSound = pref.getBoolean(SettingsFragment.SOUND, false);
-        final boolean boolVibr = pref.getBoolean(SettingsFragment.VIBR, true);
-        new Thread(new Runnable() {
-            public void run() {
-                try {
-                    DefaultHttpClient client = new DefaultHttpClient();
-                    HttpGet req = new HttpGet(Lib.SITE + "rss/");
-                    HttpResponse res = client.execute(req);
-                    HttpEntity entity = res.getEntity();
-                    BufferedReader br;
-                    String s;
-                    InputStream in = new BufferedInputStream(entity.getContent());
-                    br = new BufferedReader(new InputStreamReader(in), 1000);
+    public void onReceive(Context context, Intent intent) {
+        Intent service = new Intent(context, Service.class);
+        startWakefulService(context, service);
+    }
+
+    public static class Service extends IntentService {
+        public Service() {
+            super("Summary");
+        }
+
+        @Override
+        protected void onHandleIntent(final Intent intent) {
+            final Context context = getApplicationContext();
+            SharedPreferences pref = context.getSharedPreferences(SettingsFragment.SUMMARY, MODE_PRIVATE);
+            final int p = pref.getInt(SettingsFragment.TIME, -1);
+            if (p == -1)
+                return;
+            final boolean boolSound = pref.getBoolean(SettingsFragment.SOUND, false);
+            final boolean boolVibr = pref.getBoolean(SettingsFragment.VIBR, true);
+            try {
+                DefaultHttpClient client = new DefaultHttpClient();
+                HttpGet req = new HttpGet(Lib.SITE + "rss/");
+                HttpResponse res = client.execute(req);
+                HttpEntity entity = res.getEntity();
+                BufferedReader br;
+                String s, title, link, des;
+                InputStream in = new BufferedInputStream(entity.getContent());
+                br = new BufferedReader(new InputStreamReader(in), 1000);
+                s = br.readLine();
+                while (!s.contains("item"))
                     s = br.readLine();
-                    while (!s.contains("pubDate"))
-                        s = br.readLine();
+                title = br.readLine();
+                link = withOutTag(br.readLine());
+                des = br.readLine();
+                s = br.readLine();
+                setReceiver(context, p); //настраиваем следующую проверку
+                File f = new File(context.getFilesDir() + SummaryFragment.RSS);
+                long t = 0;
+                if (f.exists())
+                    t = f.lastModified();
+                if (t > Date.parse(withOutTag(s))) { //список в загрузке не нуждается
                     br.close();
                     in.close();
-                    File f = new File(context.getFilesDir() + SummaryFragment.RSS);
-                    if (f.exists()) {
-                        int i = s.indexOf(">") + 1;
-                        s = s.substring(i, s.indexOf("<", i));
-                        long t = Date.parse(s);
-                        if (t < f.lastModified()) {
-                            setReceiver(context, p);
-                            return;
-                        }
-                    }
-                    Intent intent = new Intent(context, MainActivity.class);
-                    intent.putExtra(SummaryFragment.RSS, true);
-                    PendingIntent piEmpty = PendingIntent.getActivity(context, 0, new Intent(), PendingIntent.FLAG_UPDATE_CURRENT);
-                    PendingIntent piSummary = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-                    NotificationManager nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-                    NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context)
-                            .setSmallIcon(R.drawable.star)
-                            .setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.mipmap.ic_launcher))
-                            .setContentTitle(context.getResources().getString(R.string.site_name))
-                            .setContentText(context.getResources().getString(R.string.appeared_new))
-                            .setTicker(context.getResources().getString(R.string.appeared_new))
-                            .setWhen(System.currentTimeMillis())
-                            .setFullScreenIntent(piEmpty, true)
-                            .setContentIntent(piSummary)
-                            .setLights(Color.GREEN, 1000, 1000)
-                            .setAutoCancel(true);
-                    if (boolSound)
-                        mBuilder.setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION));
-                    if (boolVibr)
-                        mBuilder.setVibrate(new long[]{500, 1500});
-                    nm.notify(notif_id, mBuilder.build());
-                    setReceiver(context, p);
-                } catch (Exception e) {
-                    e.printStackTrace();
+                    return;
                 }
+                final Uri notif_uri = Uri.parse(link);
+                final String notif_text = context.getResources().getString(R.string.appeared_new) + withOutTag(title);
+                BufferedWriter bw = new BufferedWriter(new FileWriter(f));
+                do {
+                    bw.write(withOutTag(title));
+                    bw.write(Lib.N);
+                    if (link.contains(Lib.SITE))
+                        bw.write(Lib.LINK + link.substring(Lib.SITE.length()));
+                    else
+                        bw.write(link);
+                    bw.write(Lib.N);
+                    bw.write(withOutTag(des));
+                    bw.write(Lib.N);
+                    bw.write(Date.parse(withOutTag(s)) + Lib.N); //time
+                    bw.flush();
+                    s = br.readLine(); //</item><item> or </channel>
+                    if (s.contains("</channel>")) break;
+                    title = br.readLine();
+                    link = withOutTag(br.readLine());
+                    des = br.readLine();
+                    s = br.readLine(); //time
+                } while (s != null);
+                bw.close();
+                br.close();
+                in.close();
+
+                Intent app = new Intent(context, SlashActivity.class);
+                app.setData(notif_uri);
+                PendingIntent piEmpty = PendingIntent.getActivity(context, 0, new Intent(), PendingIntent.FLAG_UPDATE_CURRENT);
+                PendingIntent piSummary = PendingIntent.getActivity(context, 0, app, PendingIntent.FLAG_UPDATE_CURRENT);
+                NotificationManager nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+                NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context)
+                        .setSmallIcon(R.drawable.star)
+                        .setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.mipmap.ic_launcher))
+                        .setContentTitle(context.getResources().getString(R.string.site_name))
+                        .setContentText(notif_text)
+                        .setTicker(notif_text)
+                        .setWhen(System.currentTimeMillis())
+                        .setFullScreenIntent(piEmpty, true)
+                        .setContentIntent(piSummary)
+                        .setLights(Color.GREEN, 1000, 1000)
+                        .setAutoCancel(true);
+                if (boolSound)
+                    mBuilder.setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION));
+                if (boolVibr)
+                    mBuilder.setVibrate(new long[]{500, 1500});
+                nm.notify(notif_id, mBuilder.build());
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        }).start();
+            SummaryReceiver.completeWakefulIntent(intent);
+        }
+
+        private String withOutTag(String s) {
+            int i = s.indexOf(">") + 1;
+            s = s.substring(i, s.indexOf("<", i));
+            return s;
+        }
     }
 }
