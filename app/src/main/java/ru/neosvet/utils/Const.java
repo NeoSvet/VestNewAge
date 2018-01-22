bbcddaf5af97230556bb4941806d76bfb2596060
NeoSvet <neosvet333@gmail.com>
2018-01-22 14:41:12 +0300

    убраны константы для куки

--- app/src/main/java/ru/neosvet/utils/Const.java
+++ app/src/main/java/ru/neosvet/utils/Const.java
@@ -7,10 +7,8 @@ package ru.neosvet.utils;
 public interface Const {
     int MAX_ON_PAGE = 15, TIMEOUT = 10;
     String SITE = "http://blagayavest.info/", SITE2 = "http://medicina.softlan.lclients.ru/";
-    String N = "\n", NN = "\n\n", AND = "&", BR = "<br>", KV_OPEN = "“", KV_CLOSE = "”";
+    String LINK = "<link>", N = "\n", NN = "\n\n", AND = "&", BR = "<br>", KV_OPEN = "“", KV_CLOSE = "”";
     String LIGHT = "/style/light.css", DARK = "/style/dark.css", PRINT = "?styletpl=print";
     String ENCODING = "cp1251", USER_AGENT = "User-Agent", COOKIE = "Cookie", SET_COOKIE = "Set-Cookie";
-    String SESSION_ID = "PHPSESSID", FIRST = "first", TIME_LAST_VISIT = "time_last_visit",
-            NOREAD = "noread";
-    String LINK = "<link>", TASK = "task", HREF = "href", DIALOG = "dialog", POEMS = "poems";
+    String TASK = "task", HREF = "href", DIALOG = "dialog", POEMS = "poems", NOREAD = "noread", FIRST = "first";
 }
