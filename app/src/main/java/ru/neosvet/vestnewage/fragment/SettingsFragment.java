3b53434d7d09a1e93fe8d4c49bf1e46046d28b51
NeoSvet <neosvet333@gmail.com>
2018-03-15 10:09:34 +0300

    Open option by all line

--- app/src/main/java/ru/neosvet/vestnewage/fragment/SettingsFragment.java
+++ app/src/main/java/ru/neosvet/vestnewage/fragment/SettingsFragment.java
@@ -76,7 +76,7 @@ public class SettingsFragment extends Fragment {
     }
 
     private void setViews() {
-        imgBase.setOnClickListener(new View.OnClickListener() {
+        container.findViewById(R.id.bBase).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (bPanels[PANEL_BASE]) { //if open
@@ -89,7 +89,7 @@ public class SettingsFragment extends Fragment {
                 bPanels[PANEL_BASE] = !bPanels[PANEL_BASE];
             }
         });
-        imgCheck.setOnClickListener(new View.OnClickListener() {
+        container.findViewById(R.id.bCheck).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (bPanels[PANEL_CHECK]) { //if open
@@ -102,7 +102,7 @@ public class SettingsFragment extends Fragment {
                 bPanels[PANEL_CHECK] = !bPanels[PANEL_CHECK];
             }
         });
-        imgProm.setOnClickListener(new View.OnClickListener() {
+        container.findViewById(R.id.bProm).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (bPanels[PANEL_PROM]) { //if open
