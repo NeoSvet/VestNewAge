dcf208622b9212833ecd04174d2027e0c8d46bf4
NeoSvet <neosvet333@gmail.com>
2018-05-16 12:09:25 +0300

    increase timeout
    (cherry picked from commit 440dcfe)

--- app/src/main/java/ru/neosvet/utils/Const.java
+++ app/src/main/java/ru/neosvet/utils/Const.java
@@ -5,7 +5,7 @@ package ru.neosvet.utils;
  */
 
 public interface Const {
-    int MAX_ON_PAGE = 15, TIMEOUT = 10;
+    int MAX_ON_PAGE = 15, TIMEOUT = 15;
     String SITE = "http://blagayavest.info/", SITE2 = "http://medicina.softlan.lclients.ru/";
     String LINK = "<link>", N = "\n", NN = "\n\n", AND = "&", BR = "<br>", KV_OPEN = "“", KV_CLOSE = "”";
     String LIGHT = "/style/light.css", DARK = "/style/dark.css", HTML = ".html", PRINT = "?styletpl=print";
