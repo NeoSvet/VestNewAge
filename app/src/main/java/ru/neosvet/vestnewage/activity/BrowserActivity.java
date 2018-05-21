8aaaa1802304606892a7eba3c663ad7a6127f67a
NeoSvet <neosvet333@gmail.com>
2018-05-21 16:54:10 +0300

    rename Noread to Unread
    (cherry picked from commit 36c93b2)

--- app/src/main/java/ru/neosvet/vestnewage/activity/BrowserActivity.java
+++ app/src/main/java/ru/neosvet/vestnewage/activity/BrowserActivity.java
@@ -709,9 +709,9 @@ public class BrowserActivity extends AppCompatActivity
         }
     }
 
-    public void checkNoread() {
-        Noread noread = new Noread(BrowserActivity.this);
-        noread.deleteLink(link);
+    public void checkUnread() {
+        Unread unread = new Unread(BrowserActivity.this);
+        unread.deleteLink(link);
     }
 
     public void addJournal() {
