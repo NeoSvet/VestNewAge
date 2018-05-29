508a269d5b0284d8fce8a71ae3c94c4f3885c157
NeoSvet <neosvet333@gmail.com>
2018-05-29 10:43:41 +0300

    reformat Help

--- app/src/main/java/ru/neosvet/ui/HelpAdapter.java
+++ app/src/main/java/ru/neosvet/ui/HelpAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -13,6 +14,7 @@ import java.util.List;
 import ru.neosvet.vestnewage.R;
 
 public class HelpAdapter extends BaseAdapter {
+    private final String BUTTON = "b";
     private Context context;
     private List<ListItem> data = new ArrayList<ListItem>();
 
@@ -28,6 +30,20 @@ public class HelpAdapter extends BaseAdapter {
         ListItem item = new ListItem(title);
         item.setDes(des);
         data.add(item);
+    }
+
+    public void insertItem(int index, String title, int icon) {
+        ListItem item = new ListItem(title);
+        if (icon != 0) {
+            item.setDes(BUTTON);
+            item.addLink(Integer.toString(icon));
+        }
+        data.add(index, item);
+    }
+
+    public void removeItem(int index) {
+        if (data.size() > index)
+            data.remove(index);
     }
 
     @Override
@@ -47,18 +63,26 @@ public class HelpAdapter extends BaseAdapter {
 
     @Override
     public View getView(int pos, View convertView, ViewGroup parent) {
-        if (convertView == null) {
-            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        boolean button = data.get(pos).getDes().equals(BUTTON);
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        if (button)
+            convertView = inflater.inflate(R.layout.item_menu, null);
+        else
             convertView = inflater.inflate(R.layout.item_detail, null);
-        }
         TextView tv = (TextView) convertView.findViewById(R.id.text_item);
         tv.setText(data.get(pos).getTitle());
-        tv = (TextView) convertView.findViewById(R.id.des_item);
-        tv.setText(data.get(pos).getDes());
-        if (data.get(pos).getCount() == 0)
-            tv.setVisibility(View.GONE);
-        else
-            tv.setVisibility(View.VISIBLE);
+        if (button) {
+            ImageView img = (ImageView) convertView.findViewById(R.id.image_item);
+            int icon = Integer.parseInt(data.get(pos).getLink());
+            img.setImageDrawable(context.getResources().getDrawable(icon));
+        } else {
+            tv = (TextView) convertView.findViewById(R.id.des_item);
+            tv.setText(data.get(pos).getDes());
+            if (data.get(pos).getCount() == 0)
+                tv.setVisibility(View.GONE);
+            else
+                tv.setVisibility(View.VISIBLE);
+        }
         return convertView;
     }
 }
