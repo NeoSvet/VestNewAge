c6b678913fd525584c68b326b9a1e99b8856f0b4
NeoSvet <neosvet333@gmail.com>
2018-06-09 13:09:36 +0300

    remove unused import

--- app/src/main/java/ru/neosvet/utils/NotificationHelper.java
+++ app/src/main/java/ru/neosvet/utils/NotificationHelper.java
@@ -9,7 +9,6 @@ import android.content.Context;
 import android.content.ContextWrapper;
 import android.content.Intent;
 import android.graphics.Color;
-import android.media.AudioAttributes;
 import android.os.Build;
 import android.os.Bundle;
 
