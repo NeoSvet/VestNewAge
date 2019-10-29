501d116f0a800bc55615889f628e3a895f8fcc8f
NeoSvet <neosvet333@gmail.com>
2019-10-29 14:23:30 +0300

    fix false run

--- app/src/main/java/ru/neosvet/vestnewage/receiver/SummaryReceiver.java
+++ app/src/main/java/ru/neosvet/vestnewage/receiver/SummaryReceiver.java
@@ -3,6 +3,7 @@ package ru.neosvet.vestnewage.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 
 import ru.neosvet.vestnewage.service.SummaryService;
 
@@ -11,6 +12,8 @@ public class SummaryReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
+            return;
         Intent service = new Intent(context, SummaryService.class);
         context.startService(service);
     }
