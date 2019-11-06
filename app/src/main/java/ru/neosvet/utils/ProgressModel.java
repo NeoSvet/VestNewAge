c5f56a046e4350f2911f3205d069ad366faf6df7
NeoSvet <neosvet333@gmail.com>
2019-11-06 13:02:40 +0300

    completed getModelByName

--- app/src/main/java/ru/neosvet/utils/ProgressModel.java
+++ app/src/main/java/ru/neosvet/utils/ProgressModel.java
@@ -17,6 +17,9 @@ import java.util.Set;
 import ru.neosvet.vestnewage.model.BookModel;
 import ru.neosvet.vestnewage.model.CabModel;
 import ru.neosvet.vestnewage.model.CalendarModel;
+import ru.neosvet.vestnewage.model.LoaderModel;
+import ru.neosvet.vestnewage.model.SearchModel;
+import ru.neosvet.vestnewage.model.SiteModel;
 import ru.neosvet.vestnewage.model.SlashModel;
 import ru.neosvet.vestnewage.model.SummaryModel;
 
@@ -61,8 +64,14 @@ public class ProgressModel extends AndroidViewModel {
             return SummaryModel.getInstance();
         if (name.equals(BookModel.class.getSimpleName()))
             return BookModel.getInstance();
+        if (name.equals(SiteModel.class.getSimpleName()))
+            return SiteModel.getInstance();
         if (name.equals(CabModel.class.getSimpleName()))
             return CabModel.getInstance();
+        if (name.equals(SearchModel.class.getSimpleName()))
+            return SearchModel.getInstance();
+        if (name.equals(LoaderModel.class.getSimpleName()))
+            return LoaderModel.getInstance();
         return null;
     }
 
