772bb5e6d07733372f0472a25f22150f28df078b
NeoSvet <neosvet333@gmail.com>
2020-03-25 13:49:35 +0300

    remove if(ProgressHelper.isCancelled())

--- app/src/main/java/ru/neosvet/utils/BackFragment.java
+++ app/src/main/java/ru/neosvet/utils/BackFragment.java
@@ -7,8 +7,6 @@ import ru.neosvet.vestnewage.helpers.ProgressHelper;
 public class BackFragment extends Fragment {
     public boolean onBackPressed() {
         if (ProgressHelper.isBusy()) {
-            if(ProgressHelper.isCancelled())
-                return true;
             ProgressHelper.cancelled();
             return false;
         }
