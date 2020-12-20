3447e0bdfab05f78cc5d2f34750a7b18463b7e4d
Neosvet <neosvet333@gmail.com>
2020-12-20 23:22:19 +0300

    created AdsUtils

--- app/src/main/java/ru/neosvet/vestnewage/fragment/NewFragment.java
+++ app/src/main/java/ru/neosvet/vestnewage/fragment/NewFragment.java
@@ -1,7 +1,6 @@
 package ru.neosvet.vestnewage.fragment;
 
 import android.app.Fragment;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
@@ -10,14 +9,10 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.util.List;
 
-import ru.neosvet.ui.dialogs.CustomDialog;
+import ru.neosvet.utils.AdsUtils;
 import ru.neosvet.utils.Const;
 import ru.neosvet.vestnewage.R;
 import ru.neosvet.vestnewage.activity.BrowserActivity;
@@ -28,17 +23,17 @@ import ru.neosvet.vestnewage.list.ListAdapter;
 import ru.neosvet.vestnewage.list.ListItem;
 
 public class NewFragment extends Fragment {
-    private CustomDialog alert;
     private ListAdapter adNew;
     private MainActivity act;
-    private int index_ads = -1;
     private View container, fabClear, tvEmptyNew;
+    private AdsUtils ads;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         this.container = inflater.inflate(R.layout.new_fragment, container, false);
         act = (MainActivity) getActivity();
+        ads = new AdsUtils(act);
         act.setTitle(getResources().getString(R.string.new_section));
         tvEmptyNew = this.container.findViewById(R.id.tvEmptyNew);
         initClear();
@@ -49,15 +44,15 @@ public class NewFragment extends Fragment {
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
-        outState.putInt(Const.ADS, index_ads);
+        outState.putInt(Const.ADS, ads.getIndex());
         super.onSaveInstanceState(outState);
     }
 
     private void restoreState(Bundle state) {
         if (state != null) {
-            index_ads = state.getInt(Const.ADS);
+            int index_ads = state.getInt(Const.ADS);
             if (index_ads == -1) return;
-            showAd(adNew.getItem(index_ads).getLink(), adNew.getItem(index_ads).getHead(0));
+            ads.setIndex(index_ads);
         }
     }
 
@@ -80,15 +75,6 @@ public class NewFragment extends Fragment {
                     unread.clearList();
                     unread.setBadge();
                     unread.close();
-                    File file = new File(act.getFilesDir() + File.separator + Const.ADS);
-                    if (file.exists()) {
-                        BufferedReader br = new BufferedReader(new FileReader(file));
-                        String t = br.readLine(); // читаем время последней загрузки объявлений
-                        br.close();
-                        BufferedWriter bw = new BufferedWriter(new FileWriter(file));
-                        bw.write(t); // затираем файл объявлений, оставляем лишь время загрузки
-                        bw.close();
-                    }
                     adNew.clear();
                     adNew.notifyDataSetChanged();
                     tvEmptyNew.setVisibility(View.VISIBLE);
@@ -110,14 +96,8 @@ public class NewFragment extends Fragment {
             public void onItemClick(AdapterView<?> adapterView, View view, int pos, long l) {
                 if (act.checkBusy()) return;
                 if (adNew.getItem(pos).getTitle().contains(getResources().getString(R.string.ad))) {
-                    String link = adNew.getItem(pos).getLink();
-                    String des = adNew.getItem(pos).getHead(0);
-                    if (des.equals("")) // only link
-                        act.lib.openInApps(link, null);
-                    else {
-                        index_ads = pos;
-                        showAd(link, des);
-                    }
+                    ads.setIndex(pos);
+                    ads.showAd(adNew.getItem(pos).getLink(), adNew.getItem(pos).getHead(0));
                 } else if (!adNew.getItem(pos).getLink().equals("")) {
                     BrowserActivity.openReader(act, adNew.getItem(pos).getLink(), null);
                 }
@@ -147,7 +127,6 @@ public class NewFragment extends Fragment {
             int n;
             UnreadHelper unread = new UnreadHelper(act);
             unread.setBadge();
-            StringBuilder sb;
             long time = unread.lastModified();
             if (time > 0) {
                 List<String> links = unread.getList();
@@ -160,8 +139,7 @@ public class NewFragment extends Fragment {
                         t = t.substring(0, n) + " (" + t.substring(n + 1) + ")";
                     }
                     if (s.contains(Const.POEMS))
-                        t = getResources().getString(R.string.katren) + " " +
-                                getResources().getString(R.string.from) + " " + t;
+                        t = getResources().getString(R.string.katren) + " " + getResources().getString(R.string.from) + " " + t;
                     if (s.contains("#")) {
                         t = t.replace("#", " (") + ")";
                         adNew.addItem(new ListItem(t, s.replace("#", Const.HTML + "#")));
@@ -170,43 +148,11 @@ public class NewFragment extends Fragment {
                 }
                 links.clear();
             }
-            File file = new File(act.getFilesDir() + File.separator + Const.ADS);
-            if (file.exists()) {
-                BufferedReader br = new BufferedReader(new FileReader(file));
-                br.readLine(); //time
-                final String end = "<e>";
-                while ((s = br.readLine()) != null) {
-                    if (s.contains("<t>")) {
-                        adNew.insertItem(0, new ListItem(
-                                getResources().getString(R.string.ad)
-                                        + ": " + s.substring(3)));
-                    } else if (s.contains("<u>")) {
-                        n = Integer.parseInt(s.substring(3));
-                        if (n > act.getPackageManager().getPackageInfo(act.getPackageName(), 0).versionCode) {
-                            adNew.insertItem(0, new ListItem(
-                                    getResources().getString(R.string.ad) + ": " +
-                                            getResources().getString(R.string.access_new_version)));
-                            adNew.getItem(0).addLink(
-                                    getResources().getString(R.string.url_on_app));
-                        } else {
-                            while (!s.equals(end))
-                                s = br.readLine();
-                        }
-                    } else if (s.contains("<d>")) {
-                        t = s.substring(3);
-                        s = br.readLine();
-                        while (!s.equals(end)) {
-                            t += Const.N + s;
-                            s = br.readLine();
-                        }
-                        adNew.getItem(0).addHead(t);
-                    } else if (s.contains("<l>")) {
-                        adNew.getItem(0).addLink(s.substring(3));
-                    }
-                }
-                br.close();
+            if (time < ads.getTime()) {
+                ads.loadList(adNew, true);
+                if (ads.getIndex() > -1)
+                    ads.showAd(adNew.getItem(ads.getIndex()).getLink(), adNew.getItem(ads.getIndex()).getHead(0));
             }
-            adNew.notifyDataSetChanged();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -214,35 +160,5 @@ public class NewFragment extends Fragment {
             tvEmptyNew.setVisibility(View.VISIBLE);
             fabClear.setVisibility(View.GONE);
         }
-    }
-
-    private void showAd(final String link, String des) {
-        alert = new CustomDialog(act);
-        alert.setTitle(getResources().getString(R.string.ad));
-        alert.setMessage(des);
-
-        if (link.equals("")) { // only des
-            alert.setRightButton(getResources().getString(android.R.string.ok), new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    alert.dismiss();
-                }
-            });
-        } else {
-            alert.setRightButton(getResources().getString(R.string.open_link), new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    act.lib.openInApps(link, null);
-                    alert.dismiss();
-                }
-            });
-        }
-
-        alert.show(new DialogInterface.OnDismissListener() {
-            @Override
-            public void onDismiss(DialogInterface dialogInterface) {
-                index_ads = -1;
-            }
-        });
     }
 }
