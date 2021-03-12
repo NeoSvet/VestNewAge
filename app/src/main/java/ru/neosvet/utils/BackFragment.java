09ea846c2d328417867f3f25df12925886ec27fe
Neosvet <neosvet333@gmail.com>
2021-03-12 15:31:24 +0300

    removed fields

--- app/src/main/java/ru/neosvet/utils/BackFragment.java
+++ app/src/main/java/ru/neosvet/utils/BackFragment.java
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment;
 import ru.neosvet.vestnewage.helpers.ProgressHelper;
 
 public class BackFragment extends Fragment {
-    protected boolean created = false, needLoad = false;
     public boolean onBackPressed() {
         if (ProgressHelper.isBusy()) {
             ProgressHelper.cancelled();
