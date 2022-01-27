5f9328cfe63e16a70fe3d94f326ee2f8303f8a18
Neosvet <neosvet333@gmail.com>
2022-01-27 09:58:52 +0300

    rename app

--- app/src/main/java/ru/neosvet/utils/Const.java
+++ app/src/main/java/ru/neosvet/utils/Const.java
@@ -7,7 +7,7 @@ package ru.neosvet.utils;
 public interface Const {
     int MAX_ON_PAGE = 15, TIMEOUT = 20;
     byte TURN_OFF = -1, SCREEN_MENU = 0, SCREEN_CALENDAR = 1, SCREEN_SUMMARY = 2;
-    String SITE2 = "https://chenneling.info/", mailto = "mailto:neosvet333@gmail.com?subject=Приложение «Весть Нового Века»&body=";
+    String SITE2 = "https://chenneling.info/", mailto = "mailto:neosvet333@gmail.com?subject=Приложение «Весть Века»&body=";
     String SITE = "https://blagayavest.info/", CAB_SITE = "https://www.otkroveniya.eu/", COMMA = ",", N = "\n", NN = "\n\n", AND = "&";
     String LIGHT = "/style/light.css", DARK = "/style/dark.css", BR = "<br>", KV_OPEN = "“", KV_CLOSE = "”", CHECK = "Check";
     String HTML = ".html", PRINT = "print/", PROG = "prog", MAX = "max", FROM_OTKR = "from_otkr", FINISH = "finish";
