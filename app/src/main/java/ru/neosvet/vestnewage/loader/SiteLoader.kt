2f4dafabe6203e585b2ffe2459a5fa9c1cee028d
Neosvet <neosvet333@gmail.com>
2022-05-04 09:14:47 +0300

    optimize SiteLoader

--- app/src/main/java/ru/neosvet/vestnewage/loader/SiteLoader.kt
+++ app/src/main/java/ru/neosvet/vestnewage/loader/SiteLoader.kt
@@ -2,7 +2,6 @@ package ru.neosvet.vestnewage.loader
 
 import ru.neosvet.html.PageParser
 import ru.neosvet.utils.Const
-import ru.neosvet.utils.Lib
 import ru.neosvet.utils.NeoClient
 import ru.neosvet.vestnewage.list.ListItem
 import ru.neosvet.vestnewage.loader.basic.LinksProvider
@@ -11,8 +10,9 @@ import java.io.*
 import java.util.regex.Pattern
 
 class SiteLoader(private val file: String) : LinksProvider {
-    private val patternList = Pattern.compile("\\d{4}\\.html")
-    private val list = mutableListOf<ListItem>()
+    private val patternList: Pattern by lazy {
+        Pattern.compile("\\d{4}\\.html")
+    }
 
     override fun getLinkList(): List<String> {
         val list = mutableListOf<String>()
@@ -41,60 +41,60 @@ class SiteLoader(private val file: String) : LinksProvider {
     }
 
     fun load(url: String): List<ListItem> {
-        list.clear()
-        loadList(url)
-        saveList()
+        val list = loadList(url)
+        saveList(list)
         return list
     }
 
-    private fun loadList(link: String) {
+    private fun loadList(link: String): List<ListItem> {
         val page = PageParser()
         val isSite = link == NeoClient.SITE
-        var i: Int
         if (isSite) {
             page.load(link, "page-title")
         } else {
-            i = link.lastIndexOf("/") + 1
+            val i = link.lastIndexOf("/") + 1
             val url = link.substring(0, i) + Const.PRINT + link.substring(i)
             page.load(url, "razdel")
         }
-        var s: String? = page.currentElem ?: return
+        var s: String? = page.currentElem
         var t: String
         var a: String?
         var d = StringBuilder()
+        val list = mutableListOf<ListItem>()
+        var item: ListItem? = null
         do {
             if (page.isHead) {
                 if (isSite)
-                    list.add(ListItem(page.text, true))
+                    item = ListItem(page.text, true)
                 else {
-                    setDes(d.toString())
+                    t = d.toString()
+                    if (setDes(item, t).not())
+                        list.add(ListItem(t))
                     d = StringBuilder()
-                    list.add(ListItem(page.text))
-                    addLink("", "@")
+                    item = ListItem(page.text)
+                    addLink(item, "", "@")
                 }
+                list.add(item)
             } else {
                 a = page.link
                 t = page.text
                 if (isSite) {
                     if (t.isNotEmpty() && !s!!.contains("\"#\"")) {
                         if (s.contains("&times;") || s.contains("<button>")) break
-                        if (!s.contains("<")) list[list.size - 1].des = s
-                        else list.add(ListItem(t))
-                        a?.let { addLink(t, it) }
+                        if (!s.contains("<")) item?.des = s
+                        else {
+                            item = ListItem(t)
+                            list.add(item)
+                        }
+                        a?.let { addLink(item!!, t, it) }
                     }
                 } else {
                     if (t.isEmpty()) {
                         s = page.nextItem
-                        if (s.contains("title=")) {
-                            i = s.indexOf("title=") + 7
-                            t = s.substring(i, s.indexOf("\"", i))
-                        } else if (s.contains("alt=")) {
-                            i = s.indexOf("alt=") + 5
-                            t = s.substring(i, s.indexOf("\"", i))
-                        }
+                        t = getTitleItem(s)
                         s = "<a href='$a'>$t</a><br>"
                     }
-                    a?.let { addLink(t, it) }
+                    a?.let { addLink(item!!, t, it) }
                     d.append(s)
                 }
             }
@@ -102,35 +102,48 @@ class SiteLoader(private val file: String) : LinksProvider {
             while (!page.curItem().start && s != null)
                 s = page.nextItem
         } while (s != null)
-        setDes(d.toString())
+        t = d.toString()
+        if (setDes(item, t).not())
+            list.add(ListItem(t))
         page.clear()
+        return list
     }
 
-    private fun addLink(head: String, link: String) {
+    private fun getTitleItem(s: String): String {
+        val i = when {
+            s.contains("title=") ->
+                s.indexOf("title=") + 7
+            s.contains("alt=") ->
+                s.indexOf("alt=") + 5
+            else -> return ""
+        }
+        return s.substring(i, s.indexOf("\"", i))
+    }
+
+    private fun addLink(item: ListItem, head: String, link: String) {
         var url = link
         if (url.contains("files") || url.contains(".mp3") || url.contains(".wma")
-            || url.lastIndexOf("/") == url.length - 1)
+            || url.lastIndexOf("/") == url.length - 1
+        )
             url = NeoClient.SITE + url.substring(1)
         if (url.indexOf("/") == 0) url = url.substring(1)
-        val i = list.size - 1
-        if(list[i].link == "@")
-            list[i].clear()
-        list[i].addLink(head, url)
+        if (item.link == "@")
+            item.clear()
+        item.addLink(head, url)
     }
 
-    private fun setDes(d: String) {
-        if (list.size > 0) {
-            if (d != "") {
-                val i = list.size - 1
-                if (list[i].link == "#")
-                    list.add(ListItem(d))
-                else
-                    list[i].des = d
-            }
-        }
+    private fun setDes(item: ListItem?, d: String): Boolean {
+        if (d.isEmpty())
+            return true
+        if (item == null)
+            return false
+        if (item.link == "#")
+            return false
+        item.des = d
+        return true
     }
 
-    private fun saveList() {
+    private fun saveList(list: List<ListItem>) {
         val bw = BufferedWriter(OutputStreamWriter(FileOutputStream(file)))
         for (i in list.indices) {
             bw.write(list[i].title + Const.N)
