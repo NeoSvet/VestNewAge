27201bae26d5b09d2af4a77aeb237f15f1c2b082
Neosvet <neosvet333@gmail.com>
2022-08-25 10:48:43 +0300

    create NeoSnackbar

---
+++ app/src/main/java/ru/neosvet/vestnewage/view/basic/NeoSnackbar.kt
@@ -0,0 +1,30 @@
+package ru.neosvet.vestnewage.view.basic
+
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
+import ru.neosvet.vestnewage.R
+
+class NeoSnackbar {
+    private var snackbar: Snackbar? = null
+    val isShown: Boolean
+        get() = snackbar?.isShown == true
+
+    fun show(view: View, msg: String) {
+        val context = view.context
+        snackbar = Snackbar.make(
+            view, msg,
+            Snackbar.LENGTH_LONG
+        ).setBackgroundTint(context.getColor(R.color.colorPrimary))
+            .setTextColor(context.getColor(android.R.color.white))
+            .setActionTextColor(context.getColor(R.color.colorAccentLight))
+            .setAction(android.R.string.ok) {
+                hide()
+            }
+        snackbar?.show()
+    }
+
+    fun hide() {
+        snackbar?.dismiss()
+        snackbar = null
+    }
+}
