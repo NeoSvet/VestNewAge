98e0e33af9849f24f3b8d12ce869809d6d92629e
Neosvet <neosvet333@gmail.com>
2022-11-10 15:09:21 +0300

    HTMLElem to Kotlin

---
+++ app/src/main/java/ru/neosvet/vestnewage/loader/page/HTMLElem.kt
@@ -0,0 +1,39 @@
+package ru.neosvet.vestnewage.loader.page
+
+import android.text.Html
+
+class HTMLElem {
+    var tag: String? = null
+    var par = ""
+    var start = false
+    var end = false
+    var html = ""
+        set(value) {
+            field = if (value.contains("&")) //"&#x"
+                Html.fromHtml(value).toString()
+            else if (tag == Const.LINK)
+                value.trim { it <= ' ' } else value
+        }
+
+    constructor() {}
+
+    constructor(tag: String) {
+        this.tag = tag
+        start = tag != Const.PAR
+        end = true
+    }
+
+    val code: String
+        get() = when {
+            tag == Const.LINE -> "<$tag>$html"
+            !start && end -> "</$tag>"
+            tag == Const.TEXT -> html
+            tag == Const.IMAGE -> "<$tag $par"
+            else -> {
+                val s = if (par.isEmpty()) "<$tag>$html"
+                else if (tag == Const.LINK) "<$tag href=\"$par\">$html"
+                else "<$tag $par>$html"
+                if (end) "$s</$tag>" else s
+            }
+        }
+}
