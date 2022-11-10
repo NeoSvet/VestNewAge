8466f40798be197c63a8227d359751c77d422bb5
Neosvet <neosvet333@gmail.com>
2022-11-10 15:31:59 +0300

    StyleLoader to Kotlin

--- app/src/main/java/ru/neosvet/vestnewage/loader/page/StyleLoader.kt
+++ app/src/main/java/ru/neosvet/vestnewage/loader/page/StyleLoader.kt
@@ -1,80 +1,66 @@
-package ru.neosvet.vestnewage.loader.page;
+package ru.neosvet.vestnewage.loader.page
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.OutputStreamWriter;
+import okhttp3.Request
+import ru.neosvet.vestnewage.App
+import ru.neosvet.vestnewage.network.NeoClient
+import ru.neosvet.vestnewage.network.NetConst
+import ru.neosvet.vestnewage.utils.Const
+import ru.neosvet.vestnewage.utils.Lib
+import java.io.*
 
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-import ru.neosvet.vestnewage.App;
-import ru.neosvet.vestnewage.network.NeoClient;
-import ru.neosvet.vestnewage.network.NetConst;
-import ru.neosvet.vestnewage.utils.Const;
-import ru.neosvet.vestnewage.utils.Lib;
-
-public class StyleLoader {
-    private final Request.Builder builderRequest;
-    private final OkHttpClient client;
-
-    public StyleLoader() {
-        builderRequest = new Request.Builder();
-        builderRequest.header(NetConst.USER_AGENT, App.context.getPackageName());
-        builderRequest.header("Referer", NetConst.SITE);
-        client = NeoClient.createHttpClient();
+class StyleLoader {
+    companion object {
+        private const val FONT_STRING =
+            "@font-face{font-family:\"MyriadProCondensed\";src: url(\"myriad.ttf\") format(\"truetype\")} "
     }
 
-    public void download(boolean replaceStyle) throws Exception {
-        final File fLight = Lib.getFileL(Const.LIGHT);
-        final File fDark = Lib.getFileL(Const.DARK);
-        if (!fLight.exists() || !fDark.exists() || replaceStyle) {
-            downloadStyleFromSite(fLight, fDark);
-        }
+    private val builderRequest = Request.Builder()
+    private val client = NeoClient.createHttpClient()
+
+    init {
+        builderRequest.header(NetConst.USER_AGENT, App.context.packageName)
+        builderRequest.header("Referer", NetConst.SITE)
     }
 
-    private void downloadStyleFromSite(File fLight, File fDark) throws Exception {
-        builderRequest.url(NetConst.SITE + "_content/BV/style-print.min.css");
-        Response response = client.newCall(builderRequest.build()).execute();
-        BufferedReader br = new BufferedReader(response.body().charStream(), 1000);
-        BufferedWriter bwLight = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fLight)));
-        BufferedWriter bwDark = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fDark)));
+    fun download(replaceStyle: Boolean) {
+        val fLight = Lib.getFileL(Const.LIGHT)
+        val fDark = Lib.getFileL(Const.DARK)
+        if (!fLight.exists() || !fDark.exists() || replaceStyle)
+            downloadStyleFromSite(fLight, fDark)
+    }
 
-        String s = br.readLine();
-        br.close();
-        response.close();
-        int u;
-        String[] m = s.split("#");
-        s = "@font-face{font-family:\"MyriadProCondensed\";src: url(\"myriad.ttf\") format(\"truetype\")} ";
-        bwLight.write(s);
-        bwDark.write(s);
-        bwLight.flush();
-        bwDark.flush();
-        for (int i = 1; i < m.length; i++) {
-            if (i == 1)
-                s = m[i].substring(m[i].indexOf("body"));
-            else
-                s = "#" + m[i];
+    private fun downloadStyleFromSite(fLight: File, fDark: File) {
+        builderRequest.url(NetConst.SITE + "_content/BV/style-print.min.css")
+        val response = client.newCall(builderRequest.build()).execute()
+        val br = BufferedReader(response.body.charStream(), 1000)
+        val bwLight = BufferedWriter(OutputStreamWriter(FileOutputStream(fLight)))
+        val bwDark = BufferedWriter(OutputStreamWriter(FileOutputStream(fDark)))
+        var s = br.readLine()
+        br.close()
+        response.close()
+        var u: Int
+        val m = s.split("#".toRegex())
+        bwLight.write(FONT_STRING)
+        bwDark.write(FONT_STRING)
+        bwLight.flush()
+        bwDark.flush()
+        for (i in 1 until m.size) {
+            s = if (i == 1) m[i].substring(m[i].indexOf("body")) else "#" + m[i]
             if (s.contains("P B {")) { //correct bold
-                u = s.indexOf("P B {");
-                s = s.substring(0, u) + s.substring(s.indexOf("}", u) + 1);
+                u = s.indexOf("P B {")
+                s = s.substring(0, u) + s.substring(s.indexOf("}", u) + 1)
             }
-            if (s.contains("content"))
-                s = s.replace("15px", "5px");
-            else if (s.contains("print2"))
-                s = s.replace("8pt/9pt", "12pt");
-            bwLight.write(s);
-            s = s.replace("#333", "#ccc");
-            if (s.contains("#000"))
-                s = s.replace("#000", "#fff");
-            else
-                s = s.replace("#fff", "#000");
-            bwDark.write(s);
-            bwLight.flush();
-            bwDark.flush();
+            if (s.contains("content")) s = s.replace("15px", "5px")
+            else if (s.contains("print2")) s = s.replace("8pt/9pt", "12pt")
+            bwLight.write(s)
+            s = s.replace("#333", "#ccc")
+            s = if (s.contains("#000")) s.replace("#000", "#fff")
+            else s.replace("#fff", "#000")
+            bwDark.write(s)
+            bwLight.flush()
+            bwDark.flush()
         }
-        bwLight.close();
-        bwDark.close();
+        bwLight.close()
+        bwDark.close()
     }
 }
