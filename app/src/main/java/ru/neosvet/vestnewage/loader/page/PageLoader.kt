66100e588fefeb8dd458e53b8f3acf051b6bc555
Neosvet <neosvet333@gmail.com>
2022-11-10 15:21:34 +0300

    PageLoader to Kotlin

--- app/src/main/java/ru/neosvet/vestnewage/loader/page/PageLoader.kt
+++ app/src/main/java/ru/neosvet/vestnewage/loader/page/PageLoader.kt
@@ -1,185 +1,166 @@
-package ru.neosvet.vestnewage.loader.page;
+package ru.neosvet.vestnewage.loader.page
 
-import android.content.ContentValues;
+import android.content.ContentValues
+import ru.neosvet.vestnewage.data.DataBase
+import ru.neosvet.vestnewage.data.DateUnit
+import ru.neosvet.vestnewage.loader.basic.Loader
+import ru.neosvet.vestnewage.network.NeoClient
+import ru.neosvet.vestnewage.network.NeoClient.Companion.isSiteCom
+import ru.neosvet.vestnewage.network.NetConst
+import ru.neosvet.vestnewage.storage.PageStorage
+import ru.neosvet.vestnewage.utils.Const
+import ru.neosvet.vestnewage.utils.Lib
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+class PageLoader(private val client: NeoClient) : Loader {
+    private var kRequests = 0
+    private var timeRequests: Long = 0
+    private val storage = PageStorage()
+    var isFinish = true
+        private set
 
-import ru.neosvet.vestnewage.data.DataBase;
-import ru.neosvet.vestnewage.data.DateUnit;
-import ru.neosvet.vestnewage.loader.basic.Loader;
-import ru.neosvet.vestnewage.network.NeoClient;
-import ru.neosvet.vestnewage.network.NetConst;
-import ru.neosvet.vestnewage.storage.PageStorage;
-import ru.neosvet.vestnewage.utils.Const;
-import ru.neosvet.vestnewage.utils.Lib;
-
-public class PageLoader implements Loader {
-    private final NeoClient client;
-    private int k_requests = 0;
-    private long time_requests = 0;
-    private final PageStorage storage = new PageStorage();
-    private boolean isFinish = true;
-
-    public PageLoader(NeoClient client) {
-        this.client = client;
-    }
-
-    public boolean isFinish() {
-        return isFinish;
-    }
-
-    public boolean download(String link, boolean singlePage) throws Exception {
-        isFinish = false;
+    fun download(link: String, singlePage: Boolean): Boolean {
+        isFinish = false
         // если singlePage=true, значит страницу страницу перезагружаем, а счетчики обрабатываем
-        storage.open(link, true);
+        storage.open(link, true)
         if (!singlePage && storage.existsPage(link)) {
-            return false;
+            return false
         }
-        if (storage.isDoctrine()) {
-            downloadDoctrinePage(link);
-            if (singlePage)
-                storage.close();
-            return true;
+        if (storage.isDoctrine) {
+            downloadDoctrinePage(link)
+            if (singlePage) storage.close()
+            return true
         }
-        if (!singlePage)
-            checkRequests();
-        String s = link;
-        int k = 1;
+        if (!singlePage) checkRequests()
+        var k = 1
+        var li = link
         if (link.contains("#")) {
-            k = Integer.parseInt(s.substring(s.indexOf("#") + 1));
-            s = s.substring(0, s.indexOf("#"));
-            if (link.contains("?")) s += link.substring(link.indexOf("?"));
+            k = li.substring(li.indexOf("#") + 1).toInt()
+            li = li.substring(0, li.indexOf("#"))
+            if (link.contains("?")) li += link.substring(link.indexOf("?"))
         }
-
-        int n = k;
-        boolean boolArticle = storage.isArticle();
-        PageParser page = new PageParser(client);
-        page.load(NetConst.SITE + Const.PRINT + s, "");
-        if (singlePage)
-            storage.deleteParagraphs(storage.getPageId(link));
-
-        ContentValues row;
-        int id = 0, bid = 0;
-        s = page.getCurrentElem();
+        var n = k
+        val boolArticle = storage.isArticle
+        val page = PageParser(client)
+        page.load(NetConst.SITE + Const.PRINT + li, "")
+        if (singlePage) storage.deleteParagraphs(storage.getPageId(li))
+        var row: ContentValues
+        var id = 0
+        var bid = 0
+        var s: String? = page.currentElem
         do {
-            if (page.isHead()) {
-                k--;
+            if (page.isHead) {
+                k--
                 if (k == -1 && !boolArticle) {
-                    n++;
-                    if (link.contains("#"))
-                        link = link.substring(0, link.indexOf("#"));
-                    link += "#" + n;
-                    k = 0;
+                    n++
+                    if (li.contains("#")) li = li.substring(0, li.indexOf("#"))
+                    li += "#$n"
+                    k = 0
                 }
                 if (k == 0) {
-                    id = storage.getPageId(link);
-                    row = new ContentValues();
-                    row.put(Const.TIME, System.currentTimeMillis());
-
+                    id = storage.getPageId(li)
+                    row = ContentValues()
+                    row.put(Const.TIME, System.currentTimeMillis())
                     if (id == -1) { // id не найден, материала нет - добавляем
-                        if (link.contains("#")) {
-                            id = bid;
-                            row = new ContentValues();
-                            row.put(DataBase.ID, id);
-                            row.put(DataBase.PARAGRAPH, s);
-                            storage.insertParagraph(row);
+                        if (li.contains("#")) {
+                            id = bid
+                            row = ContentValues()
+                            row.put(DataBase.ID, id)
+                            row.put(DataBase.PARAGRAPH, s)
+                            storage.insertParagraph(row)
                         } else {
-                            row.put(Const.TITLE, getTitle(s, storage.getName()));
-                            row.put(Const.LINK, link);
-                            id = (int) storage.insertTitle(row);
+                            row.put(Const.TITLE, getTitle(s, storage.name))
+                            row.put(Const.LINK, li)
+                            id = storage.insertTitle(row).toInt()
                             //обновляем дату изменения списка:
-                            row = new ContentValues();
-                            row.put(Const.TIME, System.currentTimeMillis());
-                            storage.updateTitle(1, row);
+                            row = ContentValues()
+                            row.put(Const.TIME, System.currentTimeMillis())
+                            storage.updateTitle(1, row)
                         }
                     } else { // id найден, значит материал есть
                         //обновляем заголовок
-                        row.put(Const.TITLE, getTitle(s, storage.getName()));
+                        row.put(Const.TITLE, getTitle(s, storage.name))
                         //обновляем дату загрузки материала
-                        storage.updateTitle(id, row);
+                        storage.updateTitle(id, row)
                         //удаляем содержимое материала
-                        storage.deleteParagraphs(id);
+                        storage.deleteParagraphs(id)
                     }
-                    bid = id;
-                    s = page.getNextElem();
+                    bid = id
+                    s = page.nextElem
                 }
             }
             if ((k == 0 || boolArticle) && !isEmpty(s)) {
-                row = new ContentValues();
-                row.put(DataBase.ID, id);
-                row.put(DataBase.PARAGRAPH, s);
-                storage.insertParagraph(row);
+                row = ContentValues()
+                row.put(DataBase.ID, id)
+                row.put(DataBase.PARAGRAPH, s)
+                storage.insertParagraph(row)
             }
-            s = page.getNextElem();
-        } while (s != null);
-
-        if (singlePage)
-            finish();
-        page.clear();
-        return true;
+            s = page.nextElem
+        } while (s != null)
+        if (singlePage) finish()
+        page.clear()
+        return true
     }
 
-    private void downloadDoctrinePage(String link) throws Exception {
-        String s = link.substring(Const.DOCTRINE.length()); //pages
-        String host = NeoClient.isSiteCom() ? NetConst.DOCTRINE_BASE_COM : NetConst.DOCTRINE_BASE;
-        BufferedInputStream stream = client.getStream(host + s + ".txt");
-        BufferedReader br = new BufferedReader(new InputStreamReader(stream, Const.ENCODING), 1000);
-        long time = Long.parseLong(br.readLine());
-        ContentValues row = new ContentValues();
-        row.put(Const.TIME, time);
-        storage.updateTitle(link, row);
-        int id = storage.getPageId(link);
-        storage.deleteParagraphs(id);
-        s = br.readLine();
+    private fun downloadDoctrinePage(link: String) {
+        var s: String? = link.substring(Const.DOCTRINE.length) //pages
+        val host = if (isSiteCom) NetConst.DOCTRINE_BASE_COM else NetConst.DOCTRINE_BASE
+        val stream = client.getStream("$host$s.txt")
+        val br = BufferedReader(InputStreamReader(stream, Const.ENCODING), 1000)
+        val time = br.readLine().toLong()
+        var row = ContentValues()
+        row.put(Const.TIME, time)
+        storage.updateTitle(link, row)
+        val id = storage.getPageId(link)
+        storage.deleteParagraphs(id)
+        s = br.readLine()
         while (s != null) {
-            row = new ContentValues();
-            row.put(DataBase.ID, id);
-            row.put(DataBase.PARAGRAPH, s);
-            storage.insertParagraph(row);
-            s = br.readLine();
+            row = ContentValues()
+            row.put(DataBase.ID, id)
+            row.put(DataBase.PARAGRAPH, s)
+            storage.insertParagraph(row)
+            s = br.readLine()
         }
-        br.close();
+        br.close()
     }
 
-    private boolean isEmpty(String s) {
-        return Lib.withOutTags(s).isEmpty();
+    private fun isEmpty(s: String?): Boolean {
+        return Lib.withOutTags(s).isEmpty()
     }
 
-    private void checkRequests() {
-        k_requests++;
-        if (k_requests == 5) {
-            long now = System.currentTimeMillis();
-            k_requests = 0;
-            if (now - time_requests < DateUnit.SEC_IN_MILLS) {
+    private fun checkRequests() {
+        kRequests++
+        if (kRequests == 5) {
+            val now = System.currentTimeMillis()
+            kRequests = 0
+            if (now - timeRequests < DateUnit.SEC_IN_MILLS) {
                 try {
-                    Thread.sleep(400);
-                } catch (Exception e) {
-                    e.printStackTrace();
+                    Thread.sleep(400)
+                } catch (e: Exception) {
+                    e.printStackTrace()
                 }
             }
-            time_requests = now;
+            timeRequests = now
         }
     }
 
-    private String getTitle(String line, String name) {
-        line = Lib.withOutTags(line).replace(".20", ".");
-        if (line.contains(name)) {
-            line = line.substring(9);
-            if (line.contains(Const.KV_OPEN))
-                line = line.substring(line.indexOf(Const.KV_OPEN) + 1, line.length() - 1);
+    private fun getTitle(line: String?, name: String): String {
+        var s = Lib.withOutTags(line).replace(".20", ".")
+        if (s.contains(name)) {
+            s = s.substring(9)
+            if (s.contains(Const.KV_OPEN))
+                s = s.substring(s.indexOf(Const.KV_OPEN) + 1, s.length - 1)
         }
-        return line;
+        return s
     }
 
-    public void finish() {
-        isFinish = true;
-        storage.close();
+    fun finish() {
+        isFinish = true
+        storage.close()
     }
 
-    @Override
-    public void cancel() {
-        finish();
+    override fun cancel() {
+        finish()
     }
 }
