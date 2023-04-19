2b4ad20641ac7f1a7f45d975982b99d1d9ec9bf8
Neosvet <neosvet333@gmail.com>
2023-04-19 13:54:36 +0300

    fix deprecated window feature

--- app/src/main/java/ru/neosvet/vestnewage/view/activity/BrowserActivity.kt
+++ app/src/main/java/ru/neosvet/vestnewage/view/activity/BrowserActivity.kt
@@ -97,11 +97,7 @@ class BrowserActivity : AppCompatActivity(), StateUtils.Host {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        requestWindowFeature(Window.FEATURE_NO_TITLE)
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
         binding = BrowserActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
         if (toiler.isInit.not())
