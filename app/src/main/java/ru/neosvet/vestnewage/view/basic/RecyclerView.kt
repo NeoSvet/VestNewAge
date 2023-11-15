0434daabc2583b843694878d727e898b00963b3f
Neosvet <neosvet333@gmail.com>
2023-11-15 15:09:31 +0300

    remove unused

--- app/src/main/java/ru/neosvet/vestnewage/view/basic/RecyclerView.kt
+++
@@ -1,7 +0,0 @@
-package ru.neosvet.vestnewage.view.basic
-
-import android.view.View
-import androidx.recyclerview.widget.RecyclerView
-
-fun RecyclerView.getItemView(position: Int): View =
-    findViewHolderForAdapterPosition(position)!!.itemView
